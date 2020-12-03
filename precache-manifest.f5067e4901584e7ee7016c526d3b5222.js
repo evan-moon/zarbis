80b2dc445847676d9b06a165dde236960d6db9a8
evan-moon <bboydart91@gmail.com>
2020-12-03 10:00:05 +0000

    Deploying to gh-pages from  @ d01f1a1159e72b21001d08a5128549f5299db786 🚀

---
+++ precache-manifest.f5067e4901584e7ee7016c526d3b5222.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d8fe2386cae51c3020a53b43d28abad",
+    "url": "/zarbis/index.html"
+  },
+  {
+    "revision": "50920f2aeb316299c63d",
+    "url": "/zarbis/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "b3acb9c2fff22e5894a5",
+    "url": "/zarbis/static/css/main.86aed4bd.chunk.css"
+  },
+  {
+    "revision": "50920f2aeb316299c63d",
+    "url": "/zarbis/static/js/2.d698bf14.chunk.js"
+  },
+  {
+    "revision": "0c559e1ae0a9426ead1485b442d61f0b",
+    "url": "/zarbis/static/js/2.d698bf14.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b3acb9c2fff22e5894a5",
+    "url": "/zarbis/static/js/main.b9604dc5.chunk.js"
+  },
+  {
+    "revision": "8c33de8fa878b4f9c0c5",
+    "url": "/zarbis/static/js/runtime-main.932f1d34.js"
+  },
+  {
+    "revision": "b8731b735a7ce1d721d71d3c95d3b71f",
+    "url": "/zarbis/static/media/default-background.b8731b73.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ce020e99bf028af598b02dca7af14772.js"
+  "/zarbis/precache-manifest.f5067e4901584e7ee7016c526d3b5222.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/zarbis/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
